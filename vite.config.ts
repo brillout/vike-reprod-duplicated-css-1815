b0c77b773b8e2c0b400fed8a6ba0f8fa231a80ee
Romuald Brillout <git@brillout.com>
2024-08-18 22:17:31 +0200

    workaround

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import vike from "vike/plugin";
 
 export default defineConfig({
   plugins: [vike({}), react({})],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (!id.endsWith('.css')) return
+          const name = id.split('/').pop().split('.')[0]
+          return name
+        }
+      }
+    },
+    /*
+    cssCodeSplit: false,
+    cssMinify: false
+    */
+  }
 });
